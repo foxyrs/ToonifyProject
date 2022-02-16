bb212d3ade9495d74c83dd56304c62af19b61317
kevinhuart <huart.ke@gmail.com>
2022-02-16 19:17:42 +0100

    Ajout de l'upload de fichier avec stockage sur serveur

--- application/toonifyApp/views.py
+++ application/toonifyApp/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 def index(request):
-    context = {
-        'param': "YaY"
-    }
-    return render(request, 'toonifyApp/index.html', context)
+    if request.method == 'POST' and 'image' in request.FILES:
+        img = request.FILES['image']
+        fs = FileSystemStorage()
+        filename = fs.save(img.name, img)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'toonifyApp/index.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    else:
+        return render(request, 'toonifyApp/index.html')
